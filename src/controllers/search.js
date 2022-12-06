ea268cb23cc80e0bfb11d6aefd2ede3c3596a48f
Qwerty <dorijan.dizepp@gmail.com>
2022-12-06 15:22:39 +0100

    addede route and controller for search by a key word, updated swagger api doc

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,3 +1,28 @@
-const newSearch = (req, res) => { res.json({ message: 'Test' }); };
+const mongoose = require('mongoose');
+const Article = require("../models/Article");
+
+/**
+ * funzione che restituisce tutti i prodotti che presentano nel titolo
+ * la parola chiave indicata nella url
+ */
+const newSearch = async (req, res) => { 
+    const keyWord = req.params.key;
+    if(!keyWord)
+        return res.status(400).json({code: "702", message: "missing arguments"});
+
+    const result = await Article.find({title: {'$regex': keyWord}});    
+
+    if(!result)
+        return res.status(404).json({code: "703", message: "articles not found"});
+
+    return res.status(200).json({articles: result, code: "700", message: "success"});
+};
+
+/**
+ * la funzione restituisce tutti i prodotti relativi ad una categoria
+ */
+const newSearchCateogries = async (req, res) => {
+
+};
 
 module.exports = { newSearch };
--- src/routes/search.js
+++ src/routes/search.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/search');
 
-router.post('/', searchController.newSearch);
+router.get('/?key=:key', searchController.newSearch);
 
 module.exports = router;
