fff2ed42083a62a53e2d621e47fb7ca11f599c22
Qwerty <dorijan.dizepp@gmail.com>
2022-12-09 17:08:10 +0100

    added newProduct function in controller

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -36,7 +36,43 @@ const getProduct = async (req, res) => {
 };
 
 const newProduct = async (req, res) => {
-  res.json({ message: 'Test New Product' });
+  const title = req.body.title;
+  const description = req.body.description;
+  const location = req.body.location;
+  const date = req.body.date;
+  const state = req.body.state;
+  const price = req.body.price;
+  const quantity = req.body.quantity;
+  const shipment = req.body.shipment;
+  const shipmentPrice = req.body.shipmentPrice;
+  const handDeliver = req.body.handDeliver;
+  const handDeliverZone = req.body.handDeliverZone;
+  const isPublished = req.body.isPublished;
+  const categories = req.body.categories;
+  const photos = req.body.photos;
+
+  const articolo = new Article({
+    title: title,
+    description: description,
+    location: location,
+    date: date,
+    state: state,
+    price: price,
+    quantity: quantity,
+    shipment: shipment,
+    shipmentPrice: shipmentPrice,
+    handDeliver: handDeliver,
+    handDeliverZone: handDeliverZone,
+    isPublished: isPublished,
+    categories: categories,
+    photos: photos,
+  });
+
+    articolo.save((err, data) => {
+      if(err) res.status(404).json({code: "601", message: "error database"});
+      else res.status(200).json({code: "600", message: "success"});
+    });
+
 };
 
 const deleteProduct = async (req, res) => {
