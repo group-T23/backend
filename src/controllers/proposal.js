c0e5dab28ff4eaf4dc2d06ce117ed9f5fe31939e
Andrea Piccin <andrea.piccin02@gmail.com>
2022-12-12 13:29:09 +0100

    updated proposal(s) controller after DB refactoring

--- src/controllers/proposal.js
+++ src/controllers/proposal.js
@@ -1,33 +1,34 @@
 const mongoose = require('mongoose');
 const Article = require("../models/Article");
-const User = require("../models/User");
+const Buyer = require("../models/Buyer");
+const Seller = require("../models/Seller");
 const Proposal = require("../models/Proposal");
 
 
 const create = async(req, res) => {
 
     // required params
-    if (!(eq.body.article && req.body.price && req.body.author))
+    if (!(req.body.itemId && req.body.price && req.body.authorId))
         return res.status(400).json({ code: "", massage: "missing arguments" });
     //FIXME: add error codes
 
     let valid = true;
     // article ID must exist
     if (valid)
-        valid = mongoose.Types.ObjectId.isValid(req.body.article) && (await Article.exists({ _id: req.body.article }));
+        valid = mongoose.Types.ObjectId.isValid(req.body.itemId) && (await Article.exists({ _id: req.body.itemId }));
 
     //  author (user) ID must exist
     if (valid)
-        valid = mongoose.Types.ObjectId.isValid(req.body.author) && (await User.exists({ _id: req.body.author }));
-    let author = await User.exists({ _id: req.body.author });
+        valid = mongoose.Types.ObjectId.isValid(req.body.authorId) && (await Buyer.exists({ _id: req.body.authorId }));
+    let author = await Buyer.exists({ _id: req.body.authorId });
 
     // only one proposal can exists for one author and one item
     if (valid)
-        valid = !Proposal.exists({ article: req.body.article, author: req.body.author });
+        valid = !Proposal.exists({ item_id: req.body.itemId, author_id: req.body.authorId });
 
     // the author of the proposal must be different from the item owner
     if (valid)
-        valid = !author.articles.includes(req.body.article);
+        valid = !author.items.includes(req.body.itemId);
 
 
     if (!valid)
@@ -37,7 +38,8 @@ const create = async(req, res) => {
 
 
     const p = new Proposal({
-        article: req.body.article,
+        author_id: req.body.authorId,
+        item_id: req.body.itemId,
         state: 'PENDING',
         price: req.body.price
     });
@@ -46,10 +48,13 @@ const create = async(req, res) => {
         if (err)
             return res.status(500).json({ code: "", message: "unable to create" });
         //FIXME: add error codes
+
+        return res.status(200).json({ code: "", message: "success" });
+        //FIXME: add error codes
     });
 
     //FIXME: add proposal ID in author.proposals (DB)
-    author.proposals.push(p.id);
+    /*author.proposals.push(p.id);
     author.save((err, data) => {
         if (err)
             return res.status(500).json({ code: "", message: "unable to create" });
@@ -58,30 +63,31 @@ const create = async(req, res) => {
         return res.status(200).json({ code: "", message: "success" });
         //FIXME: add error codes
     });
+    */
 };
 
 const getAllIn = async(req, res) => {
 
     // required params
-    if (!req.body.user)
+    if (!req.body.sellerId)
         return res.status(400).json({ code: "", massage: "missing arguments" });
     //FIXME: add error codes
 
 
     //user must exist
-    if (!mongoose.Types.ObjectId.isValid(req.body.user) || !(await User.exists({ _id: req.body.user })))
+    if (!mongoose.Types.ObjectId.isValid(req.body.sellerId) || !(await Seller.exists({ _id: req.body.sellerId })))
         return res.status(500).json({ code: "", message: "invalid arguments" });
     //FIXME: add error codes
 
-    //let user = await User.exists({ _id: req.body.user });
+
     let proposals = (await Proposal.find()).filter(checkIncoming);
 
     async function checkIncoming(proposal) {
-        let item = await Article.find({ _id: proposal.article });
+        let item = await Article.find({ _id: proposal.item_id });
         if (!item)
             return false;
 
-        return item.owner == req.body.user;
+        return item.owner == req.body.sellerId;
         //FIXME: owner doesn't exists in Article
     }
 
@@ -91,17 +97,19 @@ const getAllIn = async(req, res) => {
 const getAllOut = async(req, res) => {
 
     // required params
-    if (!req.body.user)
+    if (!req.body.buyerId)
         return res.status(400).json({ code: "", massage: "missing arguments" });
     //FIXME: add error codes
 
 
     //user must exist
-    if (!mongoose.Types.ObjectId.isValid(req.body.user) || !(await User.exists({ _id: req.body.user })))
+    if (!mongoose.Types.ObjectId.isValid(req.body.buyerId) || !(await Buyer.exists({ _id: req.body.buyerId })))
         return res.status(500).json({ code: "", message: "invalid arguments" });
     //FIXME: add error codes
 
-    return res.status(200).json({ proposals: user.proposals, code: "", message: "success" });
+    let proposals = await Proposal.find({ author_id: req.body.buyerId });
+
+    return res.status(200).json({ proposals: proposals, code: "", message: "success" });
 }
 
 const accept = async(req, res) => {
@@ -116,7 +124,7 @@ const accept = async(req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.id) || !(await Proposal.exists({ _id: req.id })))
         return res.status(500).json({ code: "", message: "invalid arguments" });
     //FIXME: add error codes
-    let proposal = await Proposal.exists({ _id: req.id });
+    let proposal = await Proposal.findOne({ _id: req.id });
 
     //proposal must be in 'PENDING' state
     if (proposal.state != 'PENDING')
@@ -145,7 +153,7 @@ const reject = async(req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.id) || !(await Proposal.exists({ _id: req.id })))
         return res.status(500).json({ code: "", message: "invalid arguments" });
     //FIXME: add error codes
-    let proposal = await Proposal.exists({ _id: req.id });
+    let proposal = await Proposal.findOne({ _id: req.id });
 
     //proposal must be in 'PENDING' state
     if (proposal.state != 'PENDING')
@@ -174,7 +182,7 @@ const remove = async(req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.id) || !(await Proposal.exists({ _id: req.id })))
         return res.status(500).json({ code: "", message: "invalid arguments" });
     //FIXME: add error codes
-    let proposal = await Proposal.exists({ _id: req.id });
+    let proposal = await Proposal.findOne({ _id: req.id });
 
     //proposal must be in 'PENDING' state
     if (proposal.state != 'PENDING')
@@ -190,8 +198,6 @@ const remove = async(req, res) => {
 
     //TODO: notify owner and buyer via email
 }
-
-
 
 
 module.exports = {
