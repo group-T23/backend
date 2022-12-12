12f501ac88ee5a5ab4e75208713ead714b635d99
Andrea Piccin <andrea.piccin02@gmail.com>
2022-12-12 22:19:11 +0100

    minor changes

--- src/routes/proposal.js
+++ src/routes/proposal.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const proposalController = require('../controllers/proposal');
+const { verifyAuthentication } = require('../utils/auth').default;
 
 // C - create
-router.post('/create', proposalController.create);
+router.post('/', verifyAuthentication, proposalController.create);
 
 // R - read
-router.post('/incoming', proposalController.getAllIn);
-router.post('/outgoing', proposalController.getAllOut);
+router.get('/id=:id', verifyAuthentication, proposalController.getInfo)
+router.get('/in', verifyAuthentication, proposalController.getAllIn);
+router.get('/out', verifyAuthentication, proposalController.getAllOut);
 
 // U - update
-router.put('/accept/?id=:id', proposalController.accept);
-router.put('/reject/?id=:id', proposalController.reject);
+router.put('/accept/id=:id', verifyAuthentication, proposalController.accept);
+router.put('/reject/id=:id', verifyAuthentication, proposalController.reject);
 
 // D - delete
-router.delete('/delete/?id=:id', proposalController.remove);
-
+router.delete('/?id=:id', verifyAuthentication, proposalController.remove);
 
 module.exports = router;
