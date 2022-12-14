023e4000820a6828cc4607680d687979bf95e6d6
Andrea Piccin <andrea.piccin02@gmail.com>
2022-12-14 11:07:50 +0100

    created Item and Review APIs

---
+++ src/models/Review.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.schema({
+    authorId: { type: mongoose.Types.ObjectId, required: true },
+    sellerId: { type: mongoose.Types.ObjectId, required: true },
+    title: { type: String, required: true },
+    description: { type: String },
+    rating: { type: Number, required: true },
+});
+
+const Review = mongoose.model('Review', ReviewSchema);
+
+module.exports = Review;
---
+++ src/routes/item.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const itemController = require('../controllers/item');
+const { verifyAuthentication } = require('../utils/auth');
+
+// C - create
+router.post('/', verifyAuthentication, itemController.create);
+
+// R - read
+router.get('/id=:id', itemController.getInfo);
+router.get('/username=:username', itemController.getByUser);
+router.get('/', itemController.search);
+
+// U - update
+router.put('/', verifyAuthentication, itemController.edit);
+router.put('/publish', verifyAuthentication, itemController.publish);
+router.put('/retire', verifyAuthentication, itemController.retire);
+router.put('/buy', verifyAuthentication, itemController.buy);
+
+// D - delete
+router.delete('/', verifyAuthentication, itemController.remove);
