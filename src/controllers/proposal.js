102fa13bf162c682dc3d5d9bf77a126b73a2412f
Simone-UniTN <simone.rossi-2@studenti.unitn.it>
2023-01-28 15:54:36 +0100

    added API codes to proposal

--- src/controllers/proposal.js
+++ src/controllers/proposal.js
@@ -9,7 +9,7 @@ const create = async(req, res) => {
 
     // required params
     if (!(req.body.itemId && req.body.price))
-        return res.status(400).json({ code: "", massage: "missing arguments" });
+        return res.status(400).json({ code: "1102", massage: "missing arguments" });
 
     let valid = true;
     // article ID must exist
@@ -28,7 +28,7 @@ const create = async(req, res) => {
         valid = !author.isSeller || !(await Seller.findById(author.sellerId)).items.includes(req.body.itemId);
 
     if (!valid)
-        return res.status(422).json({ code: "", message: "invalid arguments" });
+        return res.status(422).json({ code: "1103", message: "invalid arguments" });
 
     let proposal = new Proposal({
         itemId: req.body.itemId,
@@ -39,13 +39,13 @@ const create = async(req, res) => {
 
     proposal.save()
         .catch(err => {
-            return res.status(500).json({ code: "", message: "unable to create" });
+            return res.status(500).json({ code: "1101", message: "unable to create" });
         });
 
     author.proposals.push(proposal.id);
     author.save()
         .catch(err => {
-            return res.status(500).json({ code: "", message: "unable to save changes" });
+            return res.status(500).json({ code: "1101", message: "unable to save changes" });
         })
 
     const item = await Item.findOne({ id: req.body.itemId });
@@ -53,64 +53,64 @@ const create = async(req, res) => {
     seller.proposals.push(proposal.id);
     seller.save()
         .then(ok => {
-            return res.status(201).json({ code: "", message: "success" });
+            return res.status(201).json({ code: "1100", message: "success" });
         })
         .catch(err => {
-            return res.status(500).json({ code: "", message: "unable to save changes" });
+            return res.status(500).json({ code: "1101", message: "unable to save changes" });
         });
 }
 
 const getInfo = async(req, res) => {
     //id must exist
     if (!mongoose.Types.ObjectId.isValid(req.query.id) || !(await Proposal.exists({ id: req.query.id })))
-        return res.status(500).json({ code: "", message: "invalid arguments" });
+        return res.status(500).json({ code: "1103", message: "invalid arguments" });
 
     let proposal = await Proposal.findById(req.query.id);
-    return res.status(200).json({ proposal: proposal, code: "", message: "success" });
+    return res.status(200).json({ proposal: proposal, code: "1100", message: "success" });
 }
 
 const getAllIn = async(req, res) => {
     const buyer = await getAuthenticatedBuyer(req, res);
 
     if (!buyer.isSeller)
-        return res.status(422).json({ code: "", message: "invalid user type" });
+        return res.status(422).json({ code: "1103", message: "invalid user type" });
 
     const seller = await Seller.findById(buyer.sellerId);
     let proposals = await Proposal.find({ id: { $in: seller.proposals } });
 
-    return res.status(200).json({ proposals: proposals, code: "", message: "success" });
+    return res.status(200).json({ proposals: proposals, code: "1100", message: "success" });
 }
 
 const getAllOut = async(req, res) => {
     const buyer = await getAuthenticatedBuyer(req, res);
     const proposals = await Proposal.find({ authorId: buyer.id });
-    return res.status(200).json({ proposals: proposals, code: "", message: "success" });
+    return res.status(200).json({ proposals: proposals, code: "1100", message: "success" });
 }
 
 const accept = async(req, res) => {
 
     //id must exist
     if (!mongoose.Types.ObjectId.isValid(req.params.id) || !(await Proposal.exists({ id: req.params.id })))
-        return res.status(500).json({ code: "", message: "invalid arguments" });
+        return res.status(500).json({ code: "1103", message: "invalid arguments" });
     let proposal = await Proposal.findById(req.params.id);
 
     // verify authorization
     let buyer = await getAuthenticatedBuyer(req, res);
     if (!buyer.isSeller || !(await Seller.findById(buyer.sellerId)).proposals.includes(proposal.id))
-        return res.status(403).json({ code: "", message: "proposal on not owned item" })
+        return res.status(403).json({ code: "1105", message: "proposal on not owned item" })
 
     //proposal must be in 'PENDING' state
     if (proposal.state != 'PENDING')
-        return res.status(422).json({ code: "", message: "invalid proposal state" });
+        return res.status(422).json({ code: "1104", message: "invalid proposal state" });
 
     proposal.state = 'ACCEPTED';
     proposal.save()
         .then(ok => {
             //TODO: notify buyer via email
-            return res.status(200).json({ code: "", message: "success" });
+            return res.status(200).json({ code: "1100", message: "success" });
         })
         .catch(err => {
-            return res.status(500).json({ code: "", message: "unable to accept" });
+            return res.status(500).json({ code: "1101", message: "unable to accept" });
         });
 }
 
