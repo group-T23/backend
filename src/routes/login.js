9345e127bfd4ff25090897dbcb11b272208b3bb1
Simone-UniTN <simone.rossi-2@studenti.unitn.it>
2023-02-05 16:08:07 +0100

    Added forgot password

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/login');
 
 router.post('/', userController.loginUser);
+router.get('/reset', userController.resetPassword);
 
 module.exports = router;
